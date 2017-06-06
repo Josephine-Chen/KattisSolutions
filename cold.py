75a7e4e029f074b92347a8937faeb7558b247aac
Josephine Chen <jychen@g.hmc.edu>
2017-06-05 20:01:09 -0700

    Update cold.py

--- cold.py
+++ cold.py
@@ -1,5 +1,7 @@
 n = int(input())
-temps = [int(nums) for nums in input().split()]
+temps = [int(nums) for nums in input().split()] 
+# Can also use map but slower by .01 s
+# temps = map(int, input().split())
 count = 0
 for temp in temps:
     if temp < 0:
