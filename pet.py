5bfb24c1aedd63d3033ae403d756012e6a7ff9aa
Josephine Chen <jychen@g.hmc.edu>
2017-06-12 13:29:30 -0500

    Create pet.py

--- pet.py
+++ pet.py
@@ -7,3 +7,12 @@ for x in range(5):
     winner[1] = total
 winner[0] += 1
 print(winner[0], winner[1])
+
+#Another method. Above takes .02s. Below takes .03s
+contestants = list()
+for i in range(5):
+  p = sum(list(map(int, input().split())))
+  contestants.append(p)
+maxScore = max(contestants)
+winner = str(contestants.index(maxScore) + 1)
+print(winner + ' ' + str(maxScore))
