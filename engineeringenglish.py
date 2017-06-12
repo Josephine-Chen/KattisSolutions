7b0c8de488fe913dc4a7207b586b82394f267bcd
Josephine Chen <jychen@g.hmc.edu>
2017-06-12 00:18:12 -0500

    Create engineeringenglish.py

--- engineeringenglish.py
+++ engineeringenglish.py
@@ -10,3 +10,19 @@ for line in sys.stdin.readlines():
       dupe.append(n[i].lower())
       wasteless += n[i] + ' '
   print(wasteless.strip())
+
+#Another way to solve
+dupe = list()
+while True:
+  try:
+    eff = list()
+    n = input().split()
+    for i in range(len(n)):
+      if n[i] in dupe:
+        eff.append('.')
+      else:
+        dupe.append(n[i])
+        eff.append(n[i])
+    print(eff)
+  except EOFError:
+    break
