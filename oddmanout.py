6418189549a30db55ad8dc94f749000db9d9543a
Josephine Chen <jychen@g.hmc.edu>
2017-06-12 00:49:34 -0500

    Create oddmanout.py

--- oddmanout.py
+++ oddmanout.py
@@ -8,3 +8,18 @@ for i in range(n):
     if guests.count(code) == 1:
       print("Case #" + str(caseNum) + ": " + str(code))
   caseNum += 1
+
+  # Faster. Above takes 0.17s. Below takes 0.04s.
+n = int(input())
+for i in range(n):
+    g = int(input())
+    c = sorted(list(map(int, input().split())))
+    found = False
+    for j in range(0, g - 1, 2):
+        if c[j] != c[j + 1]:
+            found = True
+            break
+    if found:
+        print('Case #{}: {}'.format(i + 1, c[j]))
+    else:
+        print('Case #{}: {}'.format(i + 1, c[-1]))
