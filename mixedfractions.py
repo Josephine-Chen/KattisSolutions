64741a0dacf17286a177d02e9ac44f0b2063e0dd
Josephine Chen <jychen@g.hmc.edu>
2017-06-12 12:41:06 -0500

    Create mixedfractions.py

--- mixedfractions.py
+++ mixedfractions.py
@@ -6,3 +6,12 @@ while True:
   r = n % d
   w = int(math.floor(n / d))
   print("{} {} / {}".format(w, r, d))
+
+  # Another method. Slower. Top takes .04s, bottom takes .05s
+  
+  while True:
+  n, d = map(int, input().split())
+  if n == 0 and d == 0:
+    break
+  f = divmod(n, d)
+  print('{} {} / {}'.format(f[0], f[1], d))
